a99dbaace014cd067822a141192c3690ac3ab0c4
OMI786I <sohrawardy1998@gmail.com>
2024-06-25 16:12:34 +0600

    added auth Provider

--- src/main.jsx
+++ src/main.jsx
@@ -10,6 +10,7 @@ import AddTouristSpot from "./components/AddTouristSpot";
 import MyList from "./components/MyList";
 import Login from "./components/Login";
 import Register from "./components/Register";
+import AuthProvider from "./components/AuthProvider";
 
 const router = createBrowserRouter([
   {
@@ -46,6 +47,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </React.StrictMode>
 );
