f439257e85ebd4f3b85ddc7a0bfd9bf7e90eec86
OMI786I <sohrawardy1998@gmail.com>
2024-06-25 17:29:42 +0600

    added login with email and password

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,14 +1,37 @@
 import { FaGithub } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { FcGoogle } from "react-icons/fc";
 
 import { FaRegEye } from "react-icons/fa";
 import { FaRegEyeSlash } from "react-icons/fa";
 import toast, { Toaster } from "react-hot-toast";
-import { useState } from "react";
+import { useContext, useState } from "react";
+import { AuthContext } from "./AuthProvider";
 
 const Login = () => {
   const [showPassWord, setShowPassWord] = useState(false);
+  const [error, setError] = useState("");
+  const { signIn } = useContext(AuthContext);
+  const location = useLocation();
+  const navigate = useNavigate();
+  const handleSignIn = (e) => {
+    e.preventDefault();
+    console.log(e.currentTarget);
+    const form = new FormData(e.currentTarget);
+    const email = form.get("email");
+    const password = form.get("password");
+    signIn(email, password)
+      .then((result) => {
+        console.log(result.user);
+        navigate(location?.state ? location.state : "/");
+      })
+      .catch((error) => {
+        setError(error);
+        toast.error("Couldn't sign. Are you registered?");
+        console.error(error);
+      });
+  };
+
   return (
     <div>
       <div className="hero min-h-screen bg-base-200">
@@ -20,7 +43,7 @@ const Login = () => {
             <h1 className="text-5xl font-bold">Login now!</h1>
           </div>
           <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-            <form className="card-body">
+            <form className="card-body" onSubmit={handleSignIn}>
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Email</span>
