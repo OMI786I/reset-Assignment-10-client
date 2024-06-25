7f799358ce94e1d0188666c0c9f53819d24dc921
OMI786I <sohrawardy1998@gmail.com>
2024-06-26 04:22:23 +0600

    starting with adding of tourist spot on mongo db

--- src/components/AddTouristSpot.jsx
+++ src/components/AddTouristSpot.jsx
@@ -1,5 +1,168 @@
 const AddTouristSpot = () => {
-  return <div>This is add tourist spot</div>;
+  const handleSubmitSpot = (e) => {
+    e.preventDefault();
+    console.log(e.currentTarget);
+    const form = new FormData(e.currentTarget);
+    const email = form.get("email");
+    const password = form.get("password");
+    const name = form.get("name");
+    const photo = form.get("photo");
+    const countryName = form.get("countryName");
+    const location = form.get("location");
+    const cost = form.get("cost");
+    const description = form.get("description");
+    const season = form.get("season");
+    const time = form.get("time");
+    const visitor = form.get("visitor");
+    const spotName = form.get("spotName");
+    console.log(
+      email,
+
+      name,
+      photo,
+      countryName,
+      location,
+      description,
+      cost,
+      season,
+      time,
+      visitor,
+      spotName
+    );
+  };
+
+  return (
+    <div>
+      <form className="card-body" onSubmit={handleSubmitSpot}>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text"> Name </span>
+          </label>
+          <input
+            name="name"
+            type="text"
+            placeholder=" Name"
+            className="input input-bordered"
+          />
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Tourist Spot Name </span>
+          </label>
+          <input
+            name="spotName"
+            type="text"
+            placeholder=" Spot Name"
+            className="input input-bordered"
+          />
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text"> Country Name</span>
+          </label>
+          <input
+            name="countryName"
+            type="text"
+            placeholder="Country Name"
+            className="input input-bordered"
+          />
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text"> Location</span>
+          </label>
+          <input
+            name="location"
+            type="text"
+            placeholder="Location"
+            className="input input-bordered"
+          />
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text"> description</span>
+          </label>
+          <input
+            name="description"
+            type="text"
+            placeholder="description"
+            className="input input-bordered"
+          />
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text"> Cost</span>
+          </label>
+          <input
+            name="cost"
+            type="text"
+            placeholder="cost"
+            className="input input-bordered"
+          />
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text"> seasonality</span>
+          </label>
+          <input
+            name="season"
+            type="text"
+            placeholder="season"
+            className="input input-bordered"
+          />
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text"> travel_time</span>
+          </label>
+          <input
+            name="time"
+            type="text"
+            placeholder="travel time"
+            className="input input-bordered"
+          />
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text"> Visitors Per Year</span>
+          </label>
+          <input
+            name="visitor"
+            type="text"
+            placeholder="visitor number"
+            className="input input-bordered"
+          />
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Photo Url</span>
+          </label>
+          <input
+            name="photo"
+            type="text"
+            placeholder="image"
+            className="input input-bordered"
+          />
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Email</span>
+          </label>
+          <input
+            name="email"
+            type="email"
+            placeholder="email"
+            className="input input-bordered"
+            required
+          />
+        </div>
+
+        <div className="form-control mt-6">
+          <button className="btn btn-warning">Register</button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default AddTouristSpot;
